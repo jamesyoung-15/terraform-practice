b61d0323bcbf93737787b36eed3a21ec73364c2f
James Young <jyyoung@jyymail.com>
2024-11-07 19:04:03 -0500

    update

--- starter/main.tf
+++ starter/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  cloud {
+    organization = "jamesyoung-15"
+    workspaces {
+      name = "learn-terraform-aws"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"

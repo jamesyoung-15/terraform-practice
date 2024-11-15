4d10293b4d4386ed91dbc021dd695079405f8240
James Young <jyyoung@jyymail.com>
2024-11-14 19:46:09 -0500

    update

--- playground/s3-test/main.tf
+++ playground/s3-test/main.tf
@@ -23,3 +23,25 @@ resource "aws_s3_bucket" "my-test-bucket" {
 
   tags = local.bucket_tags
 }
+
+resource "aws_s3_bucket_policy" "test-policy" {
+  bucket = aws_s3_bucket.my-test-bucket.bucket
+  policy = "${file("s3_policy_file.json")}"
+  # policy = <<EOT
+  #     {
+  #     "Version": "2012-10-17",
+  #     "Statement": [
+  #         {
+  #             "Effect": "Allow",
+  #             "Principal": "*",
+  #             "Action": [
+  #                 "s3:GetObject",
+  #                 "s3:GetObjectVersion"
+  #             ],
+  #             "Resource": "arn:aws:s3:::tf-test-bucket-jyy151515/*"
+  #         }
+  #       ]
+  #     }
+  # EOT
+}
+
